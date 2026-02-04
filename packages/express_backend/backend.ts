9a022829129fabf8c28e978716d01db8c3f5b680
Robert-t-t <rcdiaz293@gmail.com>
2026-02-03 22:44:50 -0800

    Created a simple backend

---
+++ packages/express_backend/backend.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import type { Request, Response } from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
