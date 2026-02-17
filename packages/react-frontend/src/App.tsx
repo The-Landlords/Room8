ee69cdb51ba00b1fa2c2cf68315dbf26fa7aeb99
Emmy <ameliaharris492@gmail.com>
2026-02-17 10:04:10 -0800

    feat: implemented the basic navigation, to route your page, replace element with page component

--- packages/react-frontend/src/App.tsx
+++ packages/react-frontend/src/App.tsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+//import { useState } from "react";
+//import reactLogo from "./assets/react.svg";
+//import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<>
-			<BrowserRouter></BrowserRouter>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<h1>Sign-In</h1>} />
+					<Route path="/homeList" element={<h1>Homes</h1>} />
+					<Route path="/home" element={<h1>Contact</h1>} />
+					<Route path="/calendar" element={<h1>Calendar</h1>} />
+					<Route path="/chores" element={<h1>Chores</h1>} />
+					<Route path="/rules" element={<h1>Rules</h1>} />
+					<Route path="/settings" element={<h1>User Settings</h1>} />
+					<Route path="/grocery" element={<h1>Groceries</h1>} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
