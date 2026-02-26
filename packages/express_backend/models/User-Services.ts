08c025f915bff7fbeaef7e2b595b6f09593c7987
Robert-t-t <rcdiaz293@gmail.com>
2026-02-26 09:56:17 -0800

    Reinstalled tailwind to fix tailwind not rendering to front end pages

---
+++ packages/express_backend/models/User-Services.ts
@@ -0,0 +1,12 @@
+/*
+import { User } from "./User"
+
+function createUser(data: any){
+    return User.create(data);
+}
+
+function getApartmentById(aptId: string) {
+    return User.findById(aptId)
+}
+
+*/
