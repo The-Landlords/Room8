51a3ae95fae3b4d011fb48cf049768bfb75a8aad
Robert-t-t <rcdiaz293@gmail.com>
2026-02-26 11:03:05 -0800

    implement CRUD function

--- packages/express_backend/models/User-Services.ts
+++ packages/express_backend/models/User-Services.ts
@@ -1,9 +1,64 @@
+// user-services.ts
 import { User } from "./User";
+import mongoose from "mongoose";
 
+// CREATE
 function createUser(data: any) {
 	return User.create(data);
 }
 
-function getApartmentById(aptId: string) {
-	return User.findById(aptId);
+// READ
+function getUserById(userId: string) {
+	return User.findById(userId);
 }
+
+function getUserByUsername(username: string) {
+	return User.findOne({ username });
+}
+
+function getUsersByApartment(apartmentId: string) {
+	// users who have this apartmentId inside apartmentIds array
+	return User.find({ apartmentIds: apartmentId });
+}
+
+// UPDATE
+function updateUser(userId: string, data: any) {
+	// { new: true } returns the updated doc
+	return User.findByIdAndUpdate(userId, data, {
+		new: true,
+		runValidators: true,
+	});
+}
+
+// DELETE
+function removeUserById(userId: string) {
+	return User.findByIdAndDelete(userId);
+}
+
+// helpers for nested fields / arrays
+function addApartmentToUser(userId: string, apartmentId: string) {
+	return User.findByIdAndUpdate(
+		userId,
+		{ $addToSet: { apartmentIds: apartmentId } }, // prevents duplicates
+		{ new: true }
+	);
+}
+
+function removeApartmentFromUser(userId: string, apartmentId: string) {
+	return User.findByIdAndUpdate(
+		userId,
+		{ $pull: { apartmentIds: apartmentId } },
+		{ new: true }
+	);
+}
+
+export default {
+	createUser,
+	getUserById,
+	getUserByUsername,
+	getUsersByApartment,
+	updateUser,
+	removeUserById,
+	addApartmentToUser,
+	removeApartmentFromUser,
+};
