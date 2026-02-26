6e0963464a2e2f19f4167707d8838681c2aae778
Emmy <ameliaharris492@gmail.com>
2026-02-26 10:41:43 -0800

    renamed from apartment to home and fixed some of the home schemas

--- packages/react-frontend/src/App.tsx
+++ packages/react-frontend/src/App.tsx
@@ -2,6 +2,7 @@
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
 import "./App.css";
+import HomeList from "./homeList";
 import UserSetting from "./userSetting";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -11,7 +12,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<h1>Sign-In</h1>} />
-					<Route path="/homeList" element={<h1>Homes</h1>} />
+					<Route path="/homelist" element={<HomeList />} />
 					<Route path="/home" element={<h1>Contact</h1>} />
 					<Route path="/calendar" element={<h1>Calendar</h1>} />
 					<Route path="/chores" element={<h1>Chores</h1>} />
