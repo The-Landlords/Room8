e0d31ce97a759ee6900d73f2f038dd55801d4b49
James <smiledog781@gmail.com>
2026-03-01 02:12:53 -0800

    Added basic sign in page with tailwind

--- packages/react-frontend/src/App.tsx
+++ packages/react-frontend/src/App.tsx
@@ -5,6 +5,7 @@
 //import HomeList from "./homeList";
 import HomeList from "./homelist";
 import UserSetting from "./userSetting";
+import SignInPage from "./signInPage"; 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,8 +13,8 @@ function App() {
 		<>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<h1>Sign-In</h1>} />
-					<Route path="/homelist" element={<HomeList />} />
+                    <Route path="/" element={<SignInPage />} />           
+ 					<Route path="/homelist" element={<HomeList />} />
 					<Route path="/home" element={<h1>Contact</h1>} />
 					<Route path="/calendar" element={<h1>Calendar</h1>} />
 					<Route path="/chores" element={<h1>Chores</h1>} />
