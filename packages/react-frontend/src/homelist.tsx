2110d40f5484212a80630e57ff14cc2ab36ef1ef
Emmy <ameliaharris492@gmail.com>
2026-03-05 10:58:09 -0800

    feat: implemented the add homes panel when + button clicked

--- packages/react-frontend/src/homelist.tsx
+++ packages/react-frontend/src/homelist.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import List from "./components/list";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Overlay from "./components/overlay";
+import AddHomeOverlay from "./components/addHomeOverlay";
 
 const homes = ["Home 1", "Home 2", "Home 3", "Home 4"];
 
@@ -20,25 +21,24 @@ export default function HomeList() {
 		console.log("Closed!");
 		setOverlayOpen(false);
 	};
-	const handleSettingsClick = () => {
-		navigate("/settings");
-	};
 
 	return (
 		<div className="background-house flex flex-col items-center">
 			<h1 className="header">Home Spaces</h1>
 			<div className="iconWrapper">
-				<img
-					onClick={handleSettingsClick}
-					src="/assets/settings.png"
-					alt="Settings Icon"
-					width={60}
-					height={60}
-					className="w-20 h-20"
-				/>
+				<Link to="/settings">
+					<img
+						//onClick={handleSettingsClick}
+						src="/assets/settings.png"
+						alt="Settings Icon"
+						width={60}
+						height={60}
+						className="w-20 h-20"
+					/>
+				</Link>
 			</div>
 			<Overlay isOpen={overlayOpen} onClose={() => handleClose()}>
-				<p>Overlay</p>
+				<AddHomeOverlay />
 			</Overlay>
 			<List
 				item={homes}
