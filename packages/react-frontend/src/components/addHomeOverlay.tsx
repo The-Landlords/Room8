33f0f0a4538f9e5408aeb45362154b449e499674
Emmy <ameliaharris492@gmail.com>
2026-03-10 11:13:36 -0700

    feat: starting the different overlay options, next is to figure out how to change overlay based on user selection

--- packages/react-frontend/src/components/addHomeOverlay.tsx
+++ packages/react-frontend/src/components/addHomeOverlay.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-
+import { useState } from "react";
 export default function AddHomeOverlay() {
+	const [state, setState] = useState("");
 	return (
 		<div className="flex flex-col gap-2 animate-floatUp">
-			<h1 className="header-secondary self-center">Add Home</h1>
-			<input type="text" placeholder="Home Id" className="input-field" />
-			<button className="button self-center">Add Home</button>
+			<h1 className="header-secondary self-center">Add Options</h1>
+			<button
+				className="button self-center"
+				onClick={() => setState("Add")}
+			>
+				Add Home
+			</button>
+			<p className=" font-secondary color-secondary italic self-center">
+				Or
+			</p>
+			<button
+				className="button self-center"
+				onClick={() => setState("Create")}
+			>
+				Create Home
+			</button>
 		</div>
 	);
 }
