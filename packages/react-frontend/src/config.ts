94aec2c32a35ed34e8453edb1e895403146e4cd6
Arjan Ellingson <67170142+asellingson28@users.noreply.github.com>
2026-04-28 21:23:28 -0700

    Update config.ts

--- packages/react-frontend/src/config.ts
+++ packages/react-frontend/src/config.ts
@@ -1,4 +1,5 @@
-export const API_BASE = import.meta.env.VITE_API_URL;
+export const API_BASE =
+	"https://room8-bqgagjd0cndffae5.canadacentral-01.azurewebsites.net";
 
 if (!API_BASE) {
 	throw new Error("Missing VITE_API_URL");
