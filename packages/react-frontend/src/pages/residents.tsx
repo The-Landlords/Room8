472aa9b9a9dc7abe0b629b6935f6bb8ce9025568
Emmy <ameliaharris492@gmail.com>
2026-05-09 17:56:12 -0700

    feat: fixed residents page

--- packages/react-frontend/src/pages/residents.tsx
+++ packages/react-frontend/src/pages/residents.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import List from "../components/list";
-import { Link, useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function Residents() {
 	const [residents, setResidents] = useState<any[]>([]);
 	const { username, homeCode } = useParams();
+	const navigate = useNavigate();
 	async function fetchResidents() {
 		if (!username || !homeCode) return;
 
@@ -23,73 +24,90 @@ export default function Residents() {
 		fetchResidents().catch(console.error);
 	}, [username, homeCode]);
 	return (
-		<div>
-			<h1>Residents</h1>
-			<List
-				item="Residents"
-				items={residents}
-				handleAddClick={() => {}}
-				handleRemoveClick={() => {}}
-				renderItem={(resident) => (
-					<div className="flex flex-row gap-4">
-						<h1 className="header-secondary">
-							{resident.fullName}
-						</h1>
-						<p>
-							Allergens: {resident.allergens.join(", ") || "None"}
-						</p>
-						{resident.pronouns ? (
-							<div>
-								<p>Dislikes: {resident.pronouns || "N/A"}</p>
-							</div>
-						) : (
-							<p>Dislikes: Hidden</p>
-						)}
-						<p>Date of Birth: {resident.DOB || "N/A"}</p>
-						{resident.likes ? (
-							<div>
-								<p>
-									Likes: {resident.likes?.join(", ") || "N/A"}
-								</p>
-							</div>
-						) : (
-							<p>Likes: Hidden</p>
-						)}
-						{resident.dislikes ? (
-							<div>
-								<p>
-									Dislikes:{" "}
-									{resident.dislikes?.join(", ") || "N/A"}
-								</p>
-							</div>
-						) : (
-							<p>Dislikes: Hidden</p>
-						)}
-						{resident.emergencyContact ? (
-							<div>
-								<p>
-									Emergency Contact Name:{" "}
-									{resident.emergencyContact.name || "N/A"}
-								</p>
-								<p>
-									Emergency Contact Phone:{" "}
-									{resident.emergencyContact.phone || "N/A"}
-								</p>
-								<p>
-									Emergency Contact Relationship:{" "}
-									{resident.emergencyContact.relationship ||
-										"N/A"}
-								</p>
-							</div>
-						) : (
-							<p>Emergency Contact: Hidden</p>
-						)}
-					</div>
-				)}
-				getKey={(resident) => resident._id}
-				username={username}
-				homeCode={homeCode ? [homeCode] : undefined}
-			/>
+		<div className="flex flex-col">
+			<div className="flex justify-start">
+				<button
+					type="button"
+					onClick={() => navigate(`/homelist/${username}`)}
+					className="button h-14 w-14 flex items-center justify-center rounded-xl"
+				>
+					←
+				</button>
+			</div>
+			<div className="flex flex-col items-center">
+				<h1 className="header ">Residents</h1>
+				<List
+					item="Residents"
+					items={residents}
+					handleAddClick={() => {}}
+					handleRemoveClick={() => {}}
+					renderItem={(resident) => (
+						<div className="flex flex-row gap-4">
+							<h1 className="header-secondary">
+								{resident.fullName}
+							</h1>
+							<p>
+								Allergens:{" "}
+								{resident.allergens.join(", ") || "None"}
+							</p>
+							{resident.pronouns ? (
+								<div>
+									<p>
+										Dislikes: {resident.pronouns || "N/A"}
+									</p>
+								</div>
+							) : (
+								<p>Dislikes: Hidden</p>
+							)}
+							<p>Date of Birth: {resident.DOB || "N/A"}</p>
+							{resident.likes ? (
+								<div>
+									<p>
+										Likes:{" "}
+										{resident.likes?.join(", ") || "N/A"}
+									</p>
+								</div>
+							) : (
+								<p>Likes: Hidden</p>
+							)}
+							{resident.dislikes ? (
+								<div>
+									<p>
+										Dislikes:{" "}
+										{resident.dislikes?.join(", ") || "N/A"}
+									</p>
+								</div>
+							) : (
+								<p>Dislikes: Hidden</p>
+							)}
+							{resident.emergencyContact ? (
+								<div>
+									<p>
+										Emergency Contact Name:{" "}
+										{resident.emergencyContact.name ||
+											"N/A"}
+									</p>
+									<p>
+										Emergency Contact Phone:{" "}
+										{resident.emergencyContact.phone ||
+											"N/A"}
+									</p>
+									<p>
+										Emergency Contact Relationship:{" "}
+										{resident.emergencyContact
+											.relationship || "N/A"}
+									</p>
+								</div>
+							) : (
+								<p>Emergency Contact: Hidden</p>
+							)}
+						</div>
+					)}
+					getKey={(resident) => resident._id}
+					username={username}
+					homeCode={homeCode ? [homeCode] : undefined}
+				/>
+			</div>
 		</div>
 	);
 }
